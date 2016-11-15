e9ab8e46a93d7a4c46d7c1da37f43cddeed07c61
somasundaramr <somasundaramr@wekancode.com>
2016-11-15 18:47:43 +0530

    ConstrainLayout Scrolling Working

---
+++ app/src/main/java/testing/gps_service/sample.java
@@ -0,0 +1,15 @@
+package testing.gps_service;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import testing.gps_service.R;
+
+public class sample extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.constrain_llayout);
+    }
+}
